f6446e39e73b09560f64b9d9b917e0b6f130e072
SirePi <yamailbox@gmail.com>
2015-01-29 17:09:30 +0100

    Updated version cause nuget mishap

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Resources;
  *  by using the '*' as shown below:
  *  [assembly: AssemblyVersion("1.0.*")]
  */
-[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyVersion("1.1.1")]
 [assembly: NeutralResourcesLanguageAttribute("en-US", UltimateResourceFallbackLocation.MainAssembly)]
